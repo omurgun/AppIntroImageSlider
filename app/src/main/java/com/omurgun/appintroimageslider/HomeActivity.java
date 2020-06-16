525a1e7d8fcc01182d0d0f51ac7b80e692435360
omurgun <50786263+omurgun@users.noreply.github.com>
2020-06-16 09:34:44 +0300

    first commit

---
+++ app/src/main/java/com/omurgun/appintroimageslider/HomeActivity.java
@@ -0,0 +1,23 @@
+package com.omurgun.appintroimageslider;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        textView = findViewById(R.id.txtWelcome);
+
+    }
+
+
+}
